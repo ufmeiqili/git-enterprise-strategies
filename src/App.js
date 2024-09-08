f98210073f2c19ab140945164cba6a848cb34e7c
ufmeiqili <meili092123@gmail.com>
2024-09-08 15:41:05 -0400

    Updated application version to 1.0.1

--- src/App.js
+++ src/App.js
@@ -5,16 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
         <img src={gatorLogo} className="App-logo" alt="logo" />
-        <p> Hello World! </p>
-        <p> My name is Mei and this is my project </p>
-=======
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World! This is our team project.
-        </p>	
->>>>>>> 242861e (added new project text)
+        </p>
       </header>
     </div>
   );
